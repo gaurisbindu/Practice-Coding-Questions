3a021ca6a7ca334c57714e9170ed8269388fc459
gaurisbindu <gaurisbindu@gmail.com>
2024-11-24 01:09:52 +0530

    FindLongesWordInASentence

---
+++ FindLongestWordInASentence/SolutionOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.FindLongestWordInASentence
+{
+    internal class SolutionOne
+    {
+        public static string FindLongestWord(string sentence)
+        {
+            string longestWord = string.Empty;
+
+            string[] words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var wordWithoutSpecialChars = Regex.Replace(words[i], "[^A-Za-z]", "");
+
+                if(i== 0)
+                    longestWord = wordWithoutSpecialChars;
+
+                if (longestWord.Length < words[i].Length)
+                {
+                    longestWord = wordWithoutSpecialChars;
+                }
+            }
+
+            return longestWord;
+        }
+    }
+}
--- RotateArrayByGivenSteps/Program.cs
+++ RotateArrayByGivenSteps/Program.cs
@@ -1,47 +1,47 @@
-﻿//Question - Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+﻿////Question - Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
-//Example 1:
+////Example 1:
 
-//Input: nums = [1,2,3,4,5,6,7], k = 3
-//Output: [5,6,7,1,2,3,4]
-//Explanation:
-//rotate 1 steps to the right: [7,1,2,3,4,5,6]
-//rotate 2 steps to the right: [6,7,1,2,3,4,5]
-//rotate 3 steps to the right: [5,6,7,1,2,3,4]
+////Input: nums = [1,2,3,4,5,6,7], k = 3
+////Output: [5,6,7,1,2,3,4]
+////Explanation:
+////rotate 1 steps to the right: [7,1,2,3,4,5,6]
+////rotate 2 steps to the right: [6,7,1,2,3,4,5]
+////rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Practice_Coding_Questions.RotateArrayByGivenSteps
-{
-    internal class Program
-    {
-        static int[] Nums = [1, 2, 3, 4, 5, 6, 7];
-        static int k = 3;
+//namespace Practice_Coding_Questions.RotateArrayByGivenSteps
+//{
+//    internal class Program
+//    {
+//        static int[] Nums = [1, 2, 3, 4, 5, 6, 7];
+//        static int k = 3;
 
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Given an integer array nums, rotate the array to the right by k steps, where k is non-negative");
-            Console.Write("Input - [");
-            foreach (var item in Nums)
-            {
-                Console.Write(item + ", ");
-            }
-            Console.Write("]\n");
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Given an integer array nums, rotate the array to the right by k steps, where k is non-negative");
+//            Console.Write("Input - [");
+//            foreach (var item in Nums)
+//            {
+//                Console.Write(item + ", ");
+//            }
+//            Console.Write("]\n");
 
-            //var result = SolutionOne.RotateArrayByGivenSteps(Nums, 3);
+//            //var result = SolutionOne.RotateArrayByGivenSteps(Nums, 3);
 
-            var result = SolutionTwo_Efficient.RotateArrayByGivenSteps(Nums, 3);
+//            var result = SolutionTwo_Efficient.RotateArrayByGivenSteps(Nums, 3);
 
-            Console.Write("Output - [");
-            foreach (var item in result)
-            {
-                Console.Write(item + ", ");
-            }
-            Console.Write("]\n");
-        }
-    }
-}
+//            Console.Write("Output - [");
+//            foreach (var item in result)
+//            {
+//                Console.Write(item + ", ");
+//            }
+//            Console.Write("]\n");
+//        }
+//    }
+//}
