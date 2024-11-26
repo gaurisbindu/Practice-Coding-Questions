c2e2f443fe1c4487b2a50f2e751ffd499b975dd1
gaurisbindu <gaurisbindu@gmail.com>
2024-11-27 00:08:10 +0530

    Find longest substring without repeating characters.

---
+++ LongestSubstringWithoutRepeatingChars/Program.cs
@@ -0,0 +1,45 @@
+﻿//Question - Given a string s, find the length of the longest substring without repeating characters.
+
+//Example 1:
+
+//Input: s = "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+//Example 2:
+
+//Input: s = "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+//Example 3:
+
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+using Practice_Coding_Questions.CountPairsWithGivenDifference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.LongestSubstringWithoutRepeatingChars
+{
+    internal class Program
+    {
+        public static string Input  = "abcabcbb";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find the longest substring with non repeating characters");
+            Console.WriteLine($"Input - {Input}");
+
+            var output = SolutionTwo_Efficient.FindLongestSubstring(Input);
+
+            Console.WriteLine($"Output - {output}");
+
+        }
+    }
+
+}
---
+++ LongestSubstringWithoutRepeatingChars/SolutionTwo_Efficient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.LongestSubstringWithoutRepeatingChars
+{
+    internal class SolutionTwo_Efficient
+    {
+        // This solution is implmeneted using Sliding Window pattern
+        // Initialize :
+        // 1. 2 pointers (left & right) for the  array traversal.
+        // 2. A hashset to save unique characters to achieve time complexity of O(1) on inserts
+        // 3. A variable to store maxLength
+        // Due to sliding window, use the while loop. loop until right pointer reach its end.
+        // Insid ethe loop, if char was not in hashset, add it and move right pointer ahead.
+        // Calculate max length to maximum of previous length and current length.
+        // If char was already in the hashset, remove it from hashset, move left pointer ahead.
+        // Return maxLentgh once you are out of the loop
+
+
+        public static int FindLongestSubstring(string input)
+        {
+            int left = 0;
+            int right = 0;
+            int maxLength = 0;
+            HashSet<char> words = new();
+
+            while(right < input.Length)
+            {
+                if (!words.Contains(input[right]))
+                {
+                    words.Add(input[right]);
+                    right++;
+                    maxLength = Math.Max(maxLength, right - left);
+                }
+                else
+                {
+                    words.Remove(input[left]);
+                    left++;
+                }
+
+
+            }
+
+            return maxLength;
+        }
+    }
+}
