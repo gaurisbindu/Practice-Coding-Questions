ed9722a34fbd7463a947c8a2e3161a2e7c2d757d
gaurisbindu <gaurisbindu@gmail.com>
2024-11-26 12:52:03 +0530

    Check if array is subset of another array

---
+++ CheckIfArrayIsSubsetOfAnotherArray/SolutionOne_Naive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.CheckIfArrayIsSubsetOfAnotherArray
+{
+    internal class SolutionOne_Naive
+    {
+        public static bool CheckForArraySubset(int[] nums1, int[] nums2)
+        {
+            if (!nums1.Any() && !nums2.Any())
+                return true;
+
+            if (!nums1.Any() && nums2.Any())
+                return false;
+
+            if (nums1.Any() && !nums2.Any())
+                return true;
+
+            int[] foundAlements = new int[nums2.Length];
+            var k = 0;
+            for (int i = 0; i< nums2.Length; i++)
+            {
+                for(int j=0; j < nums1.Length; j++)
+                {
+                    if(nums2[i] == nums1[j])
+                    {
+                        foundAlements[k] = nums2[i];
+                        k++;
+                        break;
+                    }
+                }
+            }
+
+            if(k == nums2.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CheckIfArrayIsSubsetOfAnotherArray/SolutionTwo_Efficient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.CheckIfArrayIsSubsetOfAnotherArray
+{
+    internal class SolutionTwo_Efficient
+    {
+        public static bool CheckForArraySubset(int[] nums1, int[] nums2)
+        {
+            if (!nums1.Any() && !nums2.Any())
+                return true;
+
+            if (!nums1.Any() && nums2.Any())
+                return false;
+
+            if (nums1.Any() && !nums2.Any())
+                return true;
+
+            var firstArrayElements = new HashSet<int>();
+
+            foreach (var item in nums1)
+            {
+                firstArrayElements.Add(item);
+            }
+
+            var result = true;
+            foreach (var item in nums2)
+            {
+                if(!firstArrayElements.Contains(item))
+                {
+                    result = false; 
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
