9a79168f51cd8e165b5fc4d40ab652b95bc224ee
gaurisbindu <gaurisbindu@gmail.com>
2024-12-02 10:57:54 +0530

    Queue using stack under the hood

---
+++ QueueUsingStack/MyQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.QueueUsingStack
+{
+    internal class MyQueue
+    {
+        Stack<int> queue;
+        Stack<int> temporaryStack;
+        int Length;
+
+        public MyQueue()
+        {
+            queue = new Stack<int>();
+            temporaryStack = new Stack<int>();
+            Length = 0;
+        }
+
+        public void Push(int x)
+        {
+            queue.Push(x);
+            Length++;
+        }
+
+        public int? Pop()
+        {
+            if (queue.Count == 0)
+                return null;
+
+            MoveToTemporaryStack();
+            var value = temporaryStack.Pop();
+            RestoreFromTemporaryStack();
+            Length--;
+            return value;
+        }
+
+        public int? Peek()
+        {
+            if( queue.Count == 0 )
+                return null;
+
+            MoveToTemporaryStack();
+            var value = temporaryStack.Peek();
+            RestoreFromTemporaryStack() ;
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return Length == 0;
+        }
+
+        private void RestoreFromTemporaryStack()
+        {
+            while (temporaryStack.Count > 0)
+            {
+                queue.Push(temporaryStack.Pop());
+            }
+        }
+
+        private void MoveToTemporaryStack()
+        {
+            while (queue.Count > 0)
+            {
+                temporaryStack.Push(queue.Pop());
+            }
+        }
+    }
+}
---
+++ QueueUsingStack/Program.cs
@@ -0,0 +1,60 @@
+﻿//Question - Implement a first in first out (FIFO) queue using only two stacks.
+//The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+//Implement the MyQueue class:
+
+//void push(int x) Pushes element x to the back of the queue.
+//int pop() Removes the element from the front of the queue and returns it.
+//int peek() Returns the element at the front of the queue.
+//boolean empty() Returns true if the queue is empty, false otherwise.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.QueueUsingStack
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Implement Queue using Stack under the hood");
+
+            var myQueue = new MyQueue();
+            myQueue.Push(1);
+            myQueue.Push(2);
+            myQueue.Push(3);
+
+            var peek = myQueue.Peek();
+            Console.WriteLine("Peeked Item - " + peek);
+
+            var output = myQueue.Pop();
+            Console.WriteLine("Popped Item - " + output);
+
+            peek = myQueue.Peek();
+            Console.WriteLine("Peeked Item - " + peek);
+
+            output = myQueue.Pop();
+            Console.WriteLine("Popped Item - " + output);
+
+            peek = myQueue.Peek();
+            Console.WriteLine("Peeked Item - " + peek);
+
+            myQueue.Push(4);
+
+            output = myQueue.Pop();
+            Console.WriteLine("Popped Item - " + output);
+
+            peek = myQueue.Peek();
+            Console.WriteLine("Peeked Item - " + peek);
+
+            Console.WriteLine("Is Queue Empty - {0}", myQueue.IsEmpty() ? "Yes" : "No");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
