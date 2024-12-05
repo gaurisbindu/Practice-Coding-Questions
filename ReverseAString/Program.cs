dda732350d219bee821cb95fd10a9ddaee7b75f1
gaurisbindu <gaurisbindu@gmail.com>
2024-12-05 10:43:48 +0530

    Implemented Reverse string using recursion

--- ReverseAString/Program.cs
+++ ReverseAString/Program.cs
@@ -1,30 +1,37 @@
-﻿////Question - Given a string revers the string and print
-//// For Ex - "My name is Gauri" this string should be printed as "iruaG si eman yM"
+﻿//Question - Given a string revers the string and print
+// For Ex - "My name is Gauri" this string should be printed as "iruaG si eman yM"
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using Practice_Coding_Questions.MergeSortedArrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Practice_Coding_Questions.ReverseAString
-//{
-//    internal class Program
-//    {
-//        static string sample = "My name is Gauri";
+namespace Practice_Coding_Questions.ReverseAString
+{
+    internal class Program
+    {
+        static string sample = "gauri";
 
-//        public static void Main(string[] args)
-//        {
-//            Console.WriteLine("Reverse a string");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse a string");
 
-//            Console.WriteLine("Original String - " + sample);
-//            Console.Write("\nReversed string - ");
+            Console.WriteLine("Original String - " + sample);
 
-//            //Using arrays BigO(n)
-//            var result = SolutionOne_UseArrays.Reverse(sample);
+            //Using arrays BigO(n)
+            var result = SolutionOne_UseArrays.Reverse(sample);
+            
+            Console.Write("\nReversed string using Arrays - ");
+            Console.WriteLine(result);
 
-//            Console.WriteLine(result);
-//        }
+            //Using recursion  BigO(n)
+            result = SolutionTwo_UseRecursion.Reverse(sample);
 
-//    }
-//}
+            Console.Write("\nReversed string using Recursion - ");
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ ReverseAString/SolutionTwo_UseRecursion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions.ReverseAString
+{
+    internal class SolutionTwo_UseRecursion
+    {
+
+        //0 1 2 3 4
+        //----------
+        //g a u r i
+        //Reverse(gauri)
+        //Reverse(auri)
+        //Reverse(uri)
+        //Reverse(ri)
+        //Reverse(i)
+        public static string Reverse(string sample)
+        {
+            if (string.IsNullOrWhiteSpace(sample))
+                return string.Empty;
+            
+            if(sample.Length == 1) 
+                return sample;
+
+            var first = sample[0];
+            var remaining = sample.Substring(1, sample.Length - 1);
+
+            return Reverse(remaining) + first;
+        }
+    }
+}
