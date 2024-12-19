8d51ca369529a61df5b72b5965bcda9b6b9446fc
gaurisbindu <gaurisbindu@gmail.com>
2024-12-19 13:11:18 +0530

    3Sum problem naive approach

---
+++ 3Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions._3Sum
+{
+    internal class Program
+    {
+        static int[] Nums = [-1, 0, 1, 2, -1, -4];
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Given an array - Find and return unique triplet such that addition of them is 0");
+            Console.Write("Input - [");
+            foreach (var item in Nums)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.Write("]\n");
+
+            var result = SolutionOne_Naive.ThreeSum(Nums);
+
+            Console.Write("Output - [");
+            foreach (var item in result)
+            {
+                Console.Write($"[{item[0]}, {item[1]}, {item[2]}] ");
+            }
+            Console.Write("]\n");
+        }
+    }
+}
---
+++ 3Sum/SolutionOne_Naive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Coding_Questions._3Sum
+{
+    internal class SolutionOne_Naive
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+
+            var triplets = new HashSet<(int, int, int)>();
+            var length = nums.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    for (int k = j + 1; k < length; k++)
+                    {
+                        var sum = nums[i] + nums[j] + nums[k];
+                        if (sum == 0)
+                        {
+                            var temp = new List<int>() { nums[i], nums[j], nums[k] };
+                            var ordered = temp.Order().ToList();
+                            var tuple = (ordered[0], ordered[1], ordered[2]);
+
+                            triplets.Add(tuple);
+                            //Console.WriteLine("{0} {1} {2}", nums[i], nums[j], nums[k]);
+                        }
+                    }
+                }
+            }
+
+            return triplets
+                .Select(tuple => (IList<int>)new List<int> { tuple.Item1, tuple.Item2, tuple.Item3 })
+                .ToList();
+        }
+    }
+}
