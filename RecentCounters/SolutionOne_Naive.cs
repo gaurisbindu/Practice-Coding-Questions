b92515959377cf2cf2b0af290401cef15b29f7b6
gaurisbindu <gaurisbindu@gmail.com>
2024-12-23 13:53:07 +0530

    Implement Random Counter problem

---
+++ RecentCounters/SolutionOne_Naive.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice_Coding_Questions.RecentCounters
+{
+    public class SolutionOne_Naive
+    {
+
+        Queue<int> requests;
+        public SolutionOne_Naive()
+        {
+            requests = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            var lower = t - 3000;
+            var upper = t;
+
+            var answer = new Queue<int>();
+            foreach (var item in requests)
+            {
+                if (item >= lower && item <= upper)
+                {
+                    answer.Enqueue(item);
+                }
+            }
+
+            if (t >= lower && t <= upper)
+            {
+                requests.Enqueue(t);
+                answer.Enqueue(t);
+            }
+
+            var n = answer.Peek();
+           
+            return answer.Count(); // 1, 2
+        }
+    }
+}
---
+++ RecentCounters/SolutionTwo_Efficient.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_Coding_Questions.RecentCounters
+{
+    public class SolutionTwo_Efficient
+    {
+
+        Queue<int> requests;
+        public SolutionTwo_Efficient()
+        {
+            requests = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            var lower = t - 3000;
+            requests.Enqueue(t);
+
+            while(requests.Peek() < lower)
+            {
+                requests.Dequeue();
+            }
+
+            return requests.Count;
+            
+        }
+    }
+}
